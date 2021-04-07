7d80c2bc7591c6288dee10a82e81989f32a2d214
Dylan Roberts <dylangardnerroberts@gmail.com>
2021-04-07 17:27:05 -0300

    master - Added ability to create notes on the frontend

--- frontend/src/Reducer.js
+++ frontend/src/Reducer.js
@@ -3,6 +3,7 @@ import { connectRouter } from "connected-react-router";
 
 import { signupReducer } from "./components/signup/SignupReducer";
 import { loginReducer } from "./components/login/LoginReducer"; 
+import { notesReducer } from "./components/notes/NotesReducer"; 
 
 const createRootReducer = history =>
   combineReducers({
